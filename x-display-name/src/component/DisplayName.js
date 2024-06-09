4154f78a783e0120fb389ec35b823db6ce7014bc
shrwn08 <shrwnkumar08@gmail.com>
2024-06-09 15:46:15 +0530

    Completed task

--- x-display-name/src/component/DisplayName.js
+++ x-display-name/src/component/DisplayName.js
@@ -1,55 +1,42 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 const DisplayName = () => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [submit,setSubmit] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [submit, setSubmit] = useState(false);
 
+  const handChangeFirstName = (event) => {
+    setFirstName(event.target.value);
+  };
 
+  const handChangeLastName = (event) => {
+    setLastName(event.target.value);
+  };
 
-    const handChangeFirstName = (event) =>{
-        setFirstName(event.target.value);
-    }
+  const handleSubmitbtn = (event) => {
+    event.preventDefault();
+    setSubmit(true);
+    setFullName(`${firstName} ${lastName}`);
+    setFirstName('');
+    setLastName('');
+  };
 
-    const handChangeLastName = (event) =>{
-        setLastName(event.target.value);
-    }
-
-    const handleSubmitbtn=(event)=>{
-        event.preventDefault();
-            setSubmit(true)
-    }
-    const getFullName = () =>{
-        return(
-            <span>{firstName.concat(' ',lastName)}</span>
-        )
-    }
-   
   return (
     <div>
-    <h1>Full Name Display</h1>
-    <form onSubmit={handleSubmitbtn}>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmitbtn}>
         <label>First Name:&nbsp;</label>
-        <input
-        value={firstName}
-        onChange={handChangeFirstName}
-        />
-        <br/>
+        <input value={firstName} onChange={handChangeFirstName} />
+        <br />
         <label>Last Name:&nbsp;</label>
-        <input 
-        value={lastName}
-        onChange={handChangeLastName}
-        />
-        <br/>
-        <button 
-        type='submit'
-        
-        >Submit</button>
-    </form>
-    {submit?<p>Full Name:&nbsp;{getFullName()}</p>:null}
-    
+        <input value={lastName} onChange={handChangeLastName} />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <p>Full Name:&nbsp;{fullName}</p>
     </div>
-  )
-}
+  );
+};
 
-export default DisplayName
+export default DisplayName;
